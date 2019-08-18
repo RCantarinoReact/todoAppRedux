d305fac8460a1b2a162ea50e766129fb98a2f206
Renato Cantarino <renatocantarino@users.noreply.github.com>
2019-08-17 21:59:55 -0300

    Update database.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoUrl =  'mongodb+srv://cantarino:crcwifi123@cluster0-isxtc.mongodb.net/todoDB?retryWrites=true&w=majority&readPreference=secondary&ssl=true'
+const mongoUrl =  'URLBANCO'
 mongoose.Promise = global.Promise
 
 module.exports = mongoose.connect(mongoUrl, {useNewUrlParser: true})
